81bfa128dffd9a71fe8092976c4f91ac74837637
katsuro666 <kreepinoutdakut@gmail.com>
2023-12-01 16:14:26 +0300

    change main page

--- src/components/AppContent/AppContent.vue
+++ src/components/AppContent/AppContent.vue
@@ -1,6 +1,13 @@
 <template>
   <main class="app-content">
-    <div class="content-wrapper">Main page content</div>
+    <div class="content-wrapper">
+      <div class="content">
+        <h1>Welcome to the homepage of the fake app.</h1>
+        <h2>Everything here is fake. Photos, posts, even users.</h2>
+        <h3>See for yourself.</h3>
+        <h4>Welcome 😉</h4>
+      </div>
+    </div>
   </main>
 </template>
 
